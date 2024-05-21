e60ce3a00cdf8c8a1afb153751cbb9fe575f3460
Nick <niiick@live.se>
2024-05-21 22:46:59 +0200

    Add fastify cors

--- api/src/middlewares/fastify.js
+++ api/src/middlewares/fastify.js
@@ -2,10 +2,12 @@
 
 import fastifySession from '@fastify/session';
 import fastifyCookie from '@fastify/cookie';
+import fastifyCors from '@fastify/cors';
 
 export const applyFastifyPlugins = (fastify) => {
   console.info('Applying Fastify plugins...');
   fastify.register(fastifyCookie, {secret: 'C0oOkiiIiiIezZz125', secure: false});
   fastify.register(fastifySession, {secret: 'R4nDoMs3CreTofSeSioNzH3h3lolMiNThirtyTwo'});
+  fastify.register(fastifyCors, {origin: true, credentials: true});
   console.info('Fastify plugins applied.');
 };
