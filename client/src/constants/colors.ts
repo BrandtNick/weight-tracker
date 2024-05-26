85eb37c722a82e1dfc9dacf12ba8966ebfcf7a0c
Nick <niiick@live.se>
2024-05-26 18:59:01 +0200

    Some UI improvements and tweaks

--- client/src/constants/colors.ts
+++ client/src/constants/colors.ts
@@ -1,10 +1,11 @@
 export const COLORS = {
   blue: '#0e2436',
-  // blue: '#173753',
   blue2: '#143049',
   blue3: '#303f9f',
   blue4: '#9fa8da',
   blue5: '#0b1e2e',
   green: '#4caf50',
+  grey: '#424242',
+  grey2: '#d3d3d3',
   greyDarker: '#212121',
 };
--- client/src/views/weight-tracker.tsx
+++ client/src/views/weight-tracker.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {
+  Badge,
   Flex,
+  Text,
 } from '@chakra-ui/react';
 import {useQuery} from '@tanstack/react-query';
-import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts';
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from 'recharts';
+import {format, parseISO} from 'date-fns';
 
+import Spinner from '../components/spinner';
+import {COLORS} from '../constants';
 import {weightRequests} from '../api';
 
 const WeightTracker = () => {
@@ -17,26 +29,42 @@ const WeightTracker = () => {
   });
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
 
   if (!data) {
     return <div>No data</div>;
   }
-  console.log({data});
+
+  const formattedData = data.map((weight) => ({
+    ...weight,
+    timestamp: format(parseISO(weight.timestamp), 'dd/MM/yyyy'),
+  }));
 
   return (
-    <Flex>
+    <Flex flexDir='column' color={COLORS.blue4}>
+      <Flex h='200px' fontSize='.9em' align='center' justify='center'>
+        <Text>Current weight</Text> 
+        <Badge
+          colorScheme='blue'
+          color={COLORS.blue3}
+          m='10px'
+          p='5px'
+          h='28px'
+        >
+          {data[data.length - 1].weight} kg
+        </Badge>
+      </Flex>
       <LineChart
         width={900}
-        height={600}
-        data={data}
-        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        height={400}
+        data={formattedData}
+        margin={{top: 5, right: 20, bottom: 5, left: 0}}
       >
         <Line type="monotone" dataKey="weight" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <YAxis />
-        <XAxis dataKey='timestamp' />
+        <CartesianGrid stroke={COLORS.grey} strokeDasharray="5 5" />
+        <YAxis stroke={COLORS.grey2} />
+        <XAxis stroke={COLORS.grey2} dy={10} dataKey='timestamp' />
         <Tooltip />
       </LineChart>
     </Flex>
